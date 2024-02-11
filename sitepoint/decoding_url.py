d0c684d25349ea52f18c06940d01bdd23a954e32
TitoBahe <titobahe@gmail.com>
2024-02-11 21:33:56 +0200

    2.0

--- sitepoint/decoding_url.py
+++ sitepoint/decoding_url.py
@@ -1,8 +1,8 @@
-from urllib.request import urlopen 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
+from urllib.request import urlopen #brings the urllib to action
+with urlopen('http://sixty-north.com/c/t.txt') as story: #using with function for openning urls
     story_words = []
     for line in story:
-        line_story = line.decode('utf-8').split()
+        line_story = line.decode('utf-8').split() #decoding lines from binary 
         for word in line_story:
-            story_words.append(word)
+            story_words.append(word) #adding to the the list story_words 
     print(story_words)
